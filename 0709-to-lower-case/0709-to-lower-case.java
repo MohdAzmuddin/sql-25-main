3f5bbfbbae7553e78c933dac1301aea959435f8e
Mohd Azmuddin <mdazmuddinaj089@gmail.com>
2026-06-13 18:19:30 +0530

    Time: 0 ms (100%), Space: 42.7 MB (91.17%) - LeetHub

---
+++ 0709-to-lower-case/0709-to-lower-case.java
@@ -0,0 +1,18 @@
+class Solution {
+    public String toLowerCase(String s) {
+        //return s.toLowerCase();
+        //other way
+        char[] arr = s.toCharArray();
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]>='A' && arr[i]<='Z'){
+                arr[i] = (char)(arr[i]+32);//
+            }
+        }
+        String f = new String(arr);
+        return f;
+    }
+}
+// add 22 because
+//A = 65   a = 97
+//B = 66   b = 98
+// difference 97-65 = 32 therefore +32 converts an uppercase letter to lowercase.
