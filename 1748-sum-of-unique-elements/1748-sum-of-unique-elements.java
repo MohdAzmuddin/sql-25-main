60228ae933e894546bf9fa382e3f4bbc7f5c624d
Mohd Azmuddin <mdazmuddinaj089@gmail.com>
2026-08-10 19:01:55 +0530

    Time: 2 ms (57.96%), Space: 42.4 MB (99.06%) - LeetHub

--- 1748-sum-of-unique-elements/1748-sum-of-unique-elements.java
+++ 1748-sum-of-unique-elements/1748-sum-of-unique-elements.java
@@ -1,4 +1,4 @@
-class Solution {
+/*class Solution {
     public int sumOfUnique(int[] nums) {
         int sum =0;
         for(int i=0;i<nums.length;i++){
@@ -7,6 +7,22 @@ class Solution {
                 if(nums[i]==nums[j]) count++;
             }
             if(count==1) sum = sum +nums[i];
+        }
+        return sum;
+    }
+}// tc o(n2) now using HashMap tc = o(n)*/
+
+class Solution {
+    public int sumOfUnique(int[] nums) {
+        int sum =0;
+        HashMap<Integer,Integer> ans = new HashMap<>();
+
+        for(int i=0;i<nums.length;i++){
+            ans.put(nums[i],ans.getOrDefault(nums[i],0)+1);
+        }
+
+        for(int i=0;i<nums.length;i++){
+            if(ans.get(nums[i])==1) sum = sum +nums[i];
         }
         return sum;
     }
